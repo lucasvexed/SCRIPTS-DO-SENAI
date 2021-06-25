7fd5fadd19ea38f7377798bb4da0ce314f2bf550
Lucas Fava <83255809+lucasvexed@users.noreply.github.com>
2021-06-25 13:02:38 -0300

    Add files via upload

---
+++ VLSM - Exercício.c
@@ -0,0 +1,16 @@
+Alphaville - 120 hosts
+Bauru - 50 hosts
+Cotia - 30 hosts
+Campinas -14 hosts
+
+Alphavile (120 hosts) - /25 - 255.255.255.128
+Bauru (50 hosts) - /26 - 255.255.255.192
+Cotia (30 hosts) - /27 - 255.255.255.224
+Campinas (14) - /28 - 255.255.255.240
+
+Rede Principal - 193.128.0.0/24
+
+Alphaville - 193.128.0.0/25 - 128 IP's - 126 Válidos - 120 Hosts
+Bauru - 193.128.0.128/26 - 64 IP's - 62 Válidos - 50 Hosts
+Cotia - 193.128.0.192/27 - 32 IP's - 30 Válidos - 30 Hosts
+Campinas - 193.128.0.224/28 - 16 Ip's - 14 Válidos - 14 Hosts
